7095b255cef757dc017b03bce0cb98267cb2ed90
Raghda <fullasta151@gmail.com>
2023-12-10 13:39:44 +0100

    Revert "Merge branch 'main' into niyara"
    This reverts commit 8897750e1b613d476cead049f178e6d6a656bd7c, reversing
    changes made to 425aa2f500050d97b56433ba0f2c72e8d69fa8d4.

--- opendag.js
+++ opendag.js
@@ -1,15 +1,9 @@
-const container = document.querySelector(".container");
-const timeElement = document.querySelector(".time-content");
-
+// Ni-Yara - Countdown
 let countDownDate = new Date("Jan 26, 2024 00:00:00").getTime();
-
-function stopCounter() {
-  clearInterval(x);
-}
 const x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
-  
+
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -19,26 +13,12 @@ const x = setInterval(function () {
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
-  
-  
-  if (days== 0 &&hours == 0 && minutes == 0 && seconds === 0) {
-   stopCounter();
-    welcom();
-  }
 });
-function welcom() {
-  document.querySelector(".time-content").innerHTML = ''
-  let newWelcom = document.createElement("p");
-  var chang = document.getElementById("changing");
-  container.appendChild(newWelcom);
-  chang.innerHTML = 'Hurrah open day has started'
-  newWelcom.classList.add("software-dev-open");
-  
-  
-}
-// Raghda
+
+// Raghda - Welcome greeting
 let myDate = new Date();
 let hrs = myDate.getHours();
+let mins = myDate.getMinutes();
 let greet;
 console.log(hrs);
 if (hrs >= 6 && hrs <= 11.59) {
